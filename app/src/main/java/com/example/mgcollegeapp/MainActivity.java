dfeb8052fd27fb2d9870dca738605cfc88c31313
abinjomathew <abinjomathew@gmail.com>
2019-12-06 11:05:41 +0530

    button completed

--- app/src/main/java/com/example/mgcollegeapp/MainActivity.java
+++ app/src/main/java/com/example/mgcollegeapp/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.mgcollegeapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText e1,e2;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        e1=(EditText)findViewById(R.id.uname);
+        e2=(EditText)findViewById(R.id.password);
+        button=(Button)findViewById(R.id.loginbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"hello",Toast.LENGTH_LONG).show();
+
+
+            }
+        });
     }
 }
